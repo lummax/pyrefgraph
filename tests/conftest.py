c2df02034c6ae3e27144eb0933cd10a216b1a4dc
lummax <luogpg@googlemail.com>
2018-05-27 07:29:36 +0200

    run black and mypy as pytest tests

---
+++ tests/conftest.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+
+import pytest
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--runslow", action="store_true", default=False, help="run slow tests"
+    )
+
+
+def pytest_collection_modifyitems(config, items):
+    if config.getoption("--runslow"):
+        return
+    skip_slow = pytest.mark.skip(reason="need --runslow option to run")
+    for item in items:
+        if "slow" in item.keywords:
+            item.add_marker(skip_slow)
