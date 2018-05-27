c4ea42353796bcbe74d97c37c29a9a0e24e3d14b
lummax <luogpg@googlemail.com>
2018-05-27 08:53:06 +0200

    restructure source

--- reference_graph/api/__init__.py
+++
@@ -1,8 +0,0 @@
-# coding=utf-8
-import reference_graph
-
-
-def run():  # () -> reference_graph.Graph
-    graph = reference_graph.Graph()
-    graph.set_trace()
-    return graph
--- reference_graph/api/infect.py
+++
@@ -1,5 +0,0 @@
-# coding=utf-8
-import sys
-import reference_graph.api
-
-graph = reference_graph.api.run() if not sys.gettrace() else None
