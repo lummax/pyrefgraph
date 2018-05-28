5a2837441c151d55f4a634e50a21212f783f4d53
lummax <luogpg@googlemail.com>
2018-05-28 20:19:24 +0200

    setup basic import hooks

--- reference_graph/analysis/import_hook.py
+++ reference_graph/analysis/import_hook.py
@@ -1,4 +1,13 @@
 # coding=utf-8
+import sys
+import types
+import typing
+
+if sys.version_info[0] >= 3:
+    import builtins  # py3
+else:
+    import __builtin__ as builtins
+
 from reference_graph import util
 from reference_graph import graph as rgraph
 
@@ -9,11 +18,20 @@ class ImportHook(util.Setup):
         # type: (rgraph.Graph) -> None
         super(ImportHook, self).__init__()
         self.graph = graph
+        self._old_import_function = None  # type: typing.Callable
 
     def setup(self):
         # type: () -> None
         super(ImportHook, self).setup()
+        self._old_import_function = builtins.__import__
+        builtins.__import__ = self._import_function
 
     def cleanup(self):
         # type: () -> None
         super(ImportHook, self).cleanup()
+        builtins.__import__ = self._old_import_function
+
+    def _import_function(self, name, *args, **kwargs):
+        # type: (str, *typing.Any, **typing.Any) -> types.ModuleType
+        module = self._old_import_function(name, *args, **kwargs)
+        return module
