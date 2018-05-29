f78d4a670774cecefaec1370ff9f61f703bfacde
lummax <luogpg@googlemail.com>
2018-05-29 16:53:51 +0200

    also monkey patch importlib/imp/builtins.reload

--- reference_graph/analysis/import_hook.py
+++ reference_graph/analysis/import_hook.py
@@ -1,12 +1,15 @@
 # coding=utf-8
 import sys
-import types
-import typing
 
 if sys.version_info[0] >= 3:
-    import builtins  # py3
+    import builtins
+    import importlib
 else:
     import __builtin__ as builtins
+    import imp as importlib
+
+import types
+import typing
 
 from reference_graph import util
 from reference_graph import graph as rgraph
@@ -18,20 +21,82 @@ class ImportHook(util.Setup):
         # type: (rgraph.Graph) -> None
         super(ImportHook, self).__init__()
         self.graph = graph
-        self._old_import_function = None  # type: typing.Callable
+        self._cleanup_callbacks = list()  # type: typing.List[typing.Callable]
 
     def setup(self):
         # type: () -> None
         super(ImportHook, self).setup()
-        self._old_import_function = builtins.__import__
-        builtins.__import__ = self._import_function
+        self._cleanup_callbacks.extend(self._monkey_patch_import())
+        self._cleanup_callbacks.extend(self._monkey_patch_reload())
 
     def cleanup(self):
         # type: () -> None
         super(ImportHook, self).cleanup()
-        builtins.__import__ = self._old_import_function
+        while self._cleanup_callbacks:
+            function = self._cleanup_callbacks.pop(0)
+            function()
 
-    def _import_function(self, name, *args, **kwargs):
-        # type: (str, *typing.Any, **typing.Any) -> types.ModuleType
-        module = self._old_import_function(name, *args, **kwargs)
-        return module
+    def _process_module(self, name, module):
+        # type: (str, types.ModuleType) -> None
+        pass
+
+    def _monkey_patch_import(self):
+        old_import_function = builtins.__import__
+
+        def cleanup():
+            builtins.__import__ = old_import_function
+
+        def import_function(name, *args, **kwargs):
+            # type: (str, *typing.Any, **typing.Any) -> types.ModuleType
+            module = old_import_function(name, *args, **kwargs)
+            self._process_module(name, module)
+            return module
+
+        builtins.__import__ = import_function
+        yield cleanup
+
+        if hasattr(importlib, "import_module"):
+            old_importlib_import = importlib.import_module
+
+            def cleanup_importlib():
+                importlib.import_module = old_importlib_import
+
+            def importlib_import_module(name, package=None):
+                # type: (str, typing.Any) -> types.ModuleType
+                module = old_importlib_import(name, package=package)
+                self._process_module(name, module)
+                return module
+
+            importlib.import_module = importlib_import_module
+            yield cleanup_importlib
+
+    def _monkey_patch_reload(self):
+        if hasattr(builtins, "reload"):
+            old_builtins_reload = builtins.reload
+
+            def cleanup_builtins():
+                builtins.reload = old_builtins_reload
+
+            def builtins_reload(name):
+                # type: (str) -> types.ModuleType
+                module = old_builtins_reload(name)
+                self._process_module(name, module)
+                return module
+
+            builtins.reload = builtins_reload
+            yield cleanup_builtins
+
+        if hasattr(importlib, "reload"):
+            old_importlib_reload = importlib.reload
+
+            def cleanup_importlib():
+                importlib.reload = old_importlib_reload
+
+            def importlib_reload(name):
+                # type: (str) -> types.ModuleType
+                module = old_importlib_reload(name)
+                self._process_module(name, module)
+                return module
+
+            importlib.reload = importlib_reload
+            yield cleanup_importlib
