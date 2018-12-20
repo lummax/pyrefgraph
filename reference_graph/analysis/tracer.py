59d8a9ee38d20d1dab0194a8f580064363fdff25
lummax <luogpg@googlemail.com>
2018-12-20 11:38:18 +0100

    trace: add basic call tracing

--- reference_graph/analysis/tracer.py
+++ reference_graph/analysis/tracer.py
@@ -31,4 +31,37 @@ class Tracer(util.Setup):
 
     def _trace_function(self, frame, event, arg):
         # type: (types.FrameType, str, typing.Any) -> TraceFunction
+        if event == "line":
+            return self._trace_line(frame)
+        if event == "call":
+            return self._trace_call(frame)
+        if event == "return":
+            return self._trace_return(frame, arg)
+        if event == "exception":
+            return self._trace_exception(frame, *arg)
+        return self._trace_function
+
+    def _trace_line(self, frame):
+        # type: (types.FrameType) -> TraceFunction
+        return self._trace_function
+
+    def _trace_call(self, frame):
+        # type: (types.FrameType) -> TraceFunction
+        if frame.f_back:
+            self.graph.add_call(
+                self.object_manager.function_from_frame(frame.f_back.f_code),
+                self.object_manager.function_from_frame(frame.f_code),
+            )
+        else:
+            self.graph.add_entry(self.object_manager.function_from_frame(frame.f_code))
+        return self._trace_function
+
+    def _trace_return(self, frame, arg):
+        # type: (types.FrameType, typing.Any) -> TraceFunction
+        # TODO implement
+        return self._trace_function
+
+    def _trace_exception(self, frame, exception, value, traceback):
+        # type: (types.FrameType, typing.Any, typing.Any, types.TracebackType) -> TraceFunction
+        # TODO implement
         return self._trace_function
